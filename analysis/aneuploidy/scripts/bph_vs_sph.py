7c6e8de2658efba25f6e23f92812f520b12e3a21
Arjun Biddanda <aabiddanda@gmail.com>
2023-07-03 10:20:30 -0400

    draft: updated a bph vs sph script for categorizing observed trisomies

---
+++ analysis/aneuploidy/scripts/bph_vs_sph.py
@@ -0,0 +1,23 @@
+import numpy as np 
+import pickle
+import gzip as gz
+import click 
+import pandas as pd
+
+def bph(states):
+    """Identify states that are BPH - both parental homologs."""
+    pass
+
+def sph(states):
+    """Identify states that are SPH - single parental homolog."""
+    pass
+
+
+def bph_vs_sph_trisomy(gammas, states, pos, start, end):
+    """Classify BPH vs SPH for a given trisomy."""
+    pass
+
+
+if __name__ == '__main__':
+    pass
+
