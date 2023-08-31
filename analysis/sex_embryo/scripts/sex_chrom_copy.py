3046700cad0ec886b989369d02edfbaeb18cf361
Arjun Biddanda <aabiddanda@users.noreply.github.com>
2023-08-31 17:10:59 -0400

    Infer sex-chromosome karyotypes for Natera data (#8)
    * draft: implemented a draft script for estimating sex chrom karyotype
    
    * draft: added a doctored version of karyohmm to learn the sex-chrom typing
    
    * draft: now naively implements karyohmm routine for calls
    
    * draft: draft updates for actually processing data and outputting posteriors from modified karyohmm
    
    * draft: updated draft routine for sex chromosome karyotyping across embryos; uses parental genotypes
    
    * fix: environment fix for easier bcftools usage
    
    * fix: environment fix for sex-chrom inference
    
    * PR comments
    
    * add np.array to bayes factor sex_chrom_copy.py
    pickle instead of np.load
    
    * modify y chr list names
    
    * resolved click dependency for PR #8 comments.
    
    ---------
    
    Co-authored-by: scarios1 <scarios1@devrmccoy1.cm.cluster>

---
+++ analysis/sex_embryo/scripts/sex_chrom_copy.py
@@ -0,0 +1,190 @@
+import numpy as np
+import pandas as pd
+import warnings
+import gzip as gz
+from karyohmm import MetaHMM
+
+# Defining a custom set of states for chrX for copying probabilities to run through karyohmm
+chrX_states = [
+    (-1, -1, -1, -1),
+    (-1, -1, 0, -1),
+    (0, -1, -1, -1),
+    (1, -1, -1, -1),
+    (0, -1, 0, -1),
+    (1, -1, 0, -1),
+    (0, 0, -1, -1),
+    (1, 1, -1, -1),
+    (0, 1, -1, -1),
+    (0, 0, 0, -1),
+    (1, 1, 0, -1),
+    (0, 1, 0, -1),
+]
+chrX_state_names = [
+    "x0",
+    "x1p",
+    "x1m",
+    "x1m",
+    "x2",
+    "x2",
+    "x2m",
+    "x2m",
+    "x2m",
+    "x3",
+    "x3",
+    "x3",
+]
+
+chrY_states = [
+    (-1, -1, -1, -1),
+    (-1, -1, 0, -1),
+]
+chrY_state_names = ["y0", "y1"]
+
+
+def bayes_factor(posteriors, priors=None):
+    """Compute Bayes Factors for evidence of specific aneuploidy states."""
+    if priors is None:
+        priors = np.ones(posteriors.size) / posteriors.size
+
+    assert posteriors.size == priors.size
+    assert np.isclose(np.sum(priors), 1.0)
+    bfs = np.zeros(posteriors.size)
+    for i in range(posteriors.size):
+        denom = np.sum(
+            [posteriors[j] * priors[i] for j in range(posteriors.size) if j != i]
+        )
+        bfs[i] = posteriors[i] * (1 - priors[i]) / denom
+    return bfs
+
+
+def posterior_chrX_karyotype(bafs, mat_haps, pat_haps, **kwargs):
+    """Compute the posterior karyotype"""
+    assert bafs.ndim == 1
+    assert mat_haps.ndim == 2
+    assert pat_haps.ndim == 2
+    assert bafs.size == mat_haps.shape[1]
+    assert bafs.size == pat_haps.shape[1]
+    # Check for heterozygotes in the male X
+    if np.any(np.sum(pat_haps, axis=0) == 1):
+        warnings.warn(
+            "Heterozygotes observed in male chrX ... these are excluded from the overall likelihood."
+        )
+    # 1. Create the X-specific HMM
+    # NOTE: we just make the states since karyohmm isn't tailored for this just yet.
+    hmm = MetaHMM()
+    hmm.states = chrX_states
+    hmm.karyotypes = np.array(chrX_state_names, dtype=str)
+    # 2: first estimate the HMM parameters
+    pi0_est, sigma_est = hmm.est_sigma_pi0(bafs, mat_haps, pat_haps, r=1e-4)
+    # 3: run fwd-bwd decoding for the HMM
+    gammas, states, karyotypes = hmm.forward_backward(
+        bafs=bafs,
+        mat_haps=mat_haps,
+        pat_haps=pat_haps,
+        pi0=pi0_est,
+        std_dev=sigma_est,
+        unphased=True,
+    )
+    res_dict = hmm.posterior_karyotypes(gammas, karyotypes)
+    res_dict["pi0_est_chrX"] = pi0_est
+    res_dict["sigma_est_chrX"] = sigma_est
+    # 4. Obtain the maxBF and category for maxBF on chrX
+    posteriors = np.array([res_dict[x] for x in karyotypes])
+    bfs = bayes_factor(posteriors)
+    res_dict["x_maxBF"] = np.max(bfs)
+    res_dict["x_maxBF_cat"] = karyotypes[np.argmax(bfs)]
+    return res_dict
+
+
+def posterior_chrY_karyotype(bafs, pat_haps, **kwargs):
+    """Compute the log-likelihood of the embryo BAF conditional on the number of Y-chromosome copies."""
+    assert bafs.ndim == 1
+    assert pat_haps.ndim == 2
+    assert bafs.size == pat_haps.shape[1]
+    if np.any(np.sum(pat_haps, axis=0) == 1):
+        warnings.warn(
+            "Heterozygotes observed on chrY...these are excluded from the likelihood."
+        )
+    # NOTE: just create a null maternal haplotype (which we never sample anyways ...
+    mat_haps = np.zeros(shape=(2, bafs.size))
+    # 1. Setup HMM with altered state-space
+    hmm = MetaHMM()
+    hmm.states = chrY_states
+    hmm.karyotypes = np.array(chrY_state_names, dtype=str)
+    pi0_est, sigma_est = hmm.est_sigma_pi0(bafs, mat_haps, pat_haps, r=1e-4)
+    # 2. run fwd-bwd decoding for the HMM for state probabilities
+    gammas, states, karyotypes = hmm.forward_backward(
+        bafs=bafs,
+        mat_haps=mat_haps,
+        pat_haps=pat_haps,
+        pi0=pi0_est,
+        std_dev=sigma_est,
+        unphased=True,
+    )
+    # 3. Aggregate probabilities across karyotypes
+    res_dict = hmm.posterior_karyotypes(gammas, karyotypes)
+    res_dict["pi0_est_chrY"] = pi0_est
+    res_dict["sigma_est_chrY"] = sigma_est
+    # 4. Obtain the maxBF and category for maxBF for chrY
+    posteriors = np.array([res_dict[x] for x in karyotypes])
+    bfs = bayes_factor(posteriors)
+    res_dict["y_maxBF"] = np.max(bfs)
+    res_dict["y_maxBF_cat"] = karyotypes[np.argmax(bfs)]
+    return res_dict
+
+
+if __name__ == "__main__":
+    """Run the full sex-assignment method and output a TSV."""
+    # 1. Read in the BAF data for analysis
+    data = pickle.load(gz.open(snakemake.input["baf_pkl"], "r"))
+    assert "chrX" in data.keys()
+    assert "chrY" in data.keys()
+    baf_chrX_data = data["chrX"]
+    baf_chrY_data = data["chrY"]
+
+    # 2. Compute the posteriors/BF for chrX
+    n01_chrX = np.nansum(
+        (baf_chrX_data["baf_embryo"] == 1) | (baf_chrX_data["baf_embryo"] == 0)
+    )
+    m_chrX = baf_chrX_data["baf_embryo"].size
+    if n01_chrX == m_chrX:
+        print("Warning: all BAF values were either [0,1].", file=sys.stderr)
+        res_dict_chrX = {}
+        for k in chrX_state_names:
+            res_dict_chrX[x] = np.nan
+        res_dict_chrX["pi0_est_chrX"] = np.nan
+        res_dict_chrX["sigma_est_chrX"] = np.nan
+        res_dict_chrX["x_maxBF"] = np.nan
+        res_dict_chrX["x_maxBF_cat"] = np.nan
+    else:
+        res_dict_chrX = posterior_chrX_karyotype(
+            bafs=baf_chrX_data["baf_embryo"],
+            mat_haps=baf_chrX_data["mat_haps"],
+            pat_haps=baf_chrX_data["pat_haps"],
+        )
+    # 3. Compute the posteriors/BF for chrY
+    n01_chrY = np.nansum(
+        (baf_chrY_data["baf_embryo"] == 1) | (baf_chrY_data["baf_embryo"] == 0)
+    )
+    m_chrY = baf_chrY_data["baf_embryo"].size
+    if n01_chrY == m_chrY:
+        print("Warning: all BAF values were either [0,1].", file=sys.stderr)
+        res_dict_chrY = {}
+        for k in chrY_state_names:
+            res_dict_chrY[x] = np.nan
+        res_dict_chrY["pi0_est_chrY"] = np.nan
+        res_dict_chrY["sigma_est_chrY"] = np.nan
+        res_dict_chrY["y_maxBF"] = np.nan
+        res_dict_chrY["y_maxBF_cat"] = np.nan
+    else:
+        res_dict_chrY = posterior_chrY_karyotype(
+            bafs=baf_chrY_data["baf_embryo"], pat_haps=baf_chrY_data["pat_haps"]
+        )
+    # 4. Create the line for the primary output
+    with open(snakemake.output["karyo_tsv"], "w+") as out:
+        out.write(
+            "mother\tfather\tchild\tpi0_x\tsigma_x\tx0\tx1p\tx1m\tx2\tx2m\tx3\tx_maxBF\tx_maxBFcat\tpi0_y\tsigma_y\ty0\ty1\ty_maxBF\ty_maxBFcat\n"
+        )
+        # NOTE: this is kind of long and gross - and can be probably made more clear in a separate function?
+        line = f"{baf_chrX_data['mother_id']}\t{baf_chrX_data['father_id']}\t{baf_chrX_data['child_id']}\t{res_dict_chrX['pi0_est_chrX']}\t{res_dict_chrX['sigma_est_chrX']}\t{res_dict_chrX['x0']}\t{res_dict_chrX['x1p']}\t{res_dict_chrX['x1m']}\t{res_dict_chrX['x2']}\t{res_dict_chrX['x2m']}\t{res_dict_chrX['x3']}\t{res_dict_chrX['x_maxBF']}\t{res_dict_chrX['x_maxBF_cat']}\t{res_dict_chrY['pi0_est_chrY']}\t{res_dict_chrY['sigma_est_chrY']}\t{res_dict_chrY['y0']}\t{res_dict_chrY['y1']}\t{res_dict_chrY['y_maxBF']}\t{res_dict_chrY['y_maxBF_cat']}\n"
+        out.write(line)
---
+++ analysis/sex_embryo/sex_embryos.smk
@@ -0,0 +1,196 @@
+#!python3
+
+import numpy as np
+import pandas as pd
+import pickle, gzip
+from tqdm import tqdm
+from pathlib import Path
+from io import StringIO
+
+# ---- Parameters for inference in Natera Data ---- #
+metadata_file = "../../data/spectrum_metadata_merged.csv"
+alleles_file = "/data/rmccoy22/natera_spectrum/data/illumina_files/humancytosnp-12v2-1_h.update_alleles.txt"
+cluster_file = "/scratch16/rmccoy22/abiddan1/natera_spectrum/Rexpected/HumanCytoSNP-12v2-1_NS550.cluster.tsv.gz"
+meanr_file = "/scratch16/rmccoy22/abiddan1/natera_spectrum/Rexpected/meanR_child.txt"
+strand_file = "/data/rmccoy22/natera_spectrum/data/illumina_files/humancytosnp-12v2-1_h-b37.strand"
+strand_refalt = "/data/rmccoy22/natera_spectrum/data/illumina_files/humancytosnp-12v2-1_h-b37.strand.RefAlt"
+cytosnp_map_v12 = (
+    "/data/rmccoy22/natera_spectrum/data/illumina_files/snp_map_cyto12b_f004.txt"
+)
+lrrs = ["none"]
+
+# Create the VCF data dictionary for the sex-chromosomes
+vcf_dict = {}
+chroms = ["chrX", "chrY"]
+vcf_dict[
+    "chrX"
+] = "/data/rmccoy22/natera_spectrum/genotypes/opticall_parents_031423/genotypes/opticall_concat_23.norm.b38.vcf.gz"
+vcf_dict[
+    "chrY"
+] = "/data/rmccoy22/natera_spectrum/genotypes/opticall_parents_031423/genotypes/opticall_concat_24.norm.b38.vcf.gz"
+
+# Read in the aggregate metadata file
+meta_df = pd.read_csv(metadata_file)
+
+
+def find_child_data(
+    child_id, meta_df=meta_df, raw_data_path="/data/rmccoy22/natera_spectrum/data/"
+):
+    """Find the child csv file based on the provided meta_df."""
+    child_year = meta_df[meta_df.array == child_id].year.values
+    for year in child_year:
+        child_fp = Path(f"{raw_data_path}{year}/{child_id}.csv.gz")
+        if child_fp.is_file():
+            return child_fp, True
+        else:
+            continue
+    return None, False
+
+
+def create_trios(
+    meta_df, sample_file, raw_data_path="/data/rmccoy22/natera_spectrum/data/"
+):
+    """Create a list of valid trio datasets."""
+    valid_trios = []
+    unique_mothers = np.unique(
+        meta_df[meta_df.family_position == "mother"].array.values
+    )
+    for m in tqdm(unique_mothers):
+        cases = np.unique(meta_df[meta_df.array == m].casefile_id.values)
+        cur_df = meta_df[np.isin(meta_df.casefile_id, cases)]
+        fathers = np.unique(cur_df[cur_df.family_position == "father"].array.values)
+        if fathers.size > 1:
+            print(f"More than one partner for {m}")
+            for fat in fathers:
+                cur_cases = np.unique(meta_df[meta_df.array == fat].casefile_id.values)
+                cur_df = meta_df[np.isin(meta_df.casefile_id, cur_cases)]
+                for c in cur_df[cur_df.family_position == "child"].array.values:
+                    valid_trios.append((m, fat, c))
+        elif fathers.size == 1:
+            for c in cur_df[cur_df.family_position == "child"].array.values:
+                valid_trios.append((m, fathers[0], c))
+    parents = [line.rstrip() for line in open(sample_file, "r")]
+    # Applies a set of filters here
+    valid_filt_trios = []
+    for m, f, c in tqdm(valid_trios):
+        if (
+            (m in parents)
+            and (f in parents)
+            and find_child_data(c, meta_df, raw_data_path)[1]
+        ):
+            valid_filt_trios.append((m, f, c))
+
+    return valid_filt_trios
+
+
+total_data = []
+if Path("results/natera_inference/valid_trios.txt").is_file():
+    with open("results/natera_inference/valid_trios.txt", "r") as fp:
+        for i, line in enumerate(fp):
+            [m, f, c] = line.rstrip().split()
+            total_data.append(
+                f"results/natera_inference/{m}+{f}/{c}.sex_chroms.total_ploidy.tsv"
+            )
+
+
+# ------- Rules Section ------- #
+localrules:
+    all,
+
+
+rule all:
+    input:
+        "results/natera_inference/valid_trios.txt",
+        total_data,
+
+
+rule generate_parent_sample_list:
+    """Generate the parental samples list."""
+    input:
+        vcf=[vcf_dict[c] for c in chroms],
+    output:
+        "results/natera_inference/parent_samples.txt",
+    resources:
+        time="0:30:00",
+        mem_mb="1G",
+    shell:
+        "bcftools query -l {input.vcf} | sort | uniq > {output}"
+
+
+rule obtain_valid_trios:
+    """Obtain the valid trios here."""
+    input:
+        metadata_tsv=metadata_file,
+        parent_samples="results/natera_inference/parent_samples.txt",
+    output:
+        valid_trios="results/natera_inference/valid_trios.txt",
+    resources:
+        time="0:30:00",
+        mem_mb="1G",
+    run:
+        valid_trios = create_trios(
+            meta_df,
+            input.parent_samples,
+            raw_data_path="/data/rmccoy22/natera_spectrum/data/",
+        )
+        with open(output.valid_trios, "w") as out:
+            for m, f, c in valid_trios:
+                out.write(f"{m}\t{f}\t{c}\n")
+
+
+rule preprocess_baf_data_sex_chrom:
+    """Preprocess the BAF data for a trio estimating posteriors downstream
+
+    This rule performs the following steps:
+
+    1. Assess that the csv.gz corresponding to the proper embryo has the correct columns
+
+    2. Obtain the two diploid (phased) genomes for the mother & father at variants with MAF > 0.01 for speed
+
+    Then for each variant on the chromosome perform:
+
+    *  Assess if an allele is a valid SNP nucleotide (ACGT)
+
+    * Assign BAF to be reflective of the alternative allele frequency in the parental VCF (accounting for the complement strand)
+
+    * Filter out positions where either parent has a missing genotype
+    
+    The specific steps can be found in `preprocess_sex_chroms.py` in greater detail with code examples as well.
+    """
+    input:
+        metadata_csv=metadata_file,
+        cytosnp_map=cytosnp_map_v12,
+        alleles_file=alleles_file,
+        egt_cluster=cluster_file,
+        meanr_file=meanr_file,
+        vcf_file=[vcf_dict[c] for c in chroms],
+        child_data=lambda wildcards: find_child_data(wildcards.child_id)[0],
+    output:
+        baf_pkl="results/natera_inference/{mother_id}+{father_id}/{child_id}.sex_chroms.bafs.pkl.gz",
+    resources:
+        time="1:00:00",
+        mem_mb="5G",
+    wildcard_constraints:
+        chrom="|".join(chroms),
+    params:
+        chroms=chroms,
+    script:
+        "scripts/preprocess_sex_chroms.py"
+
+
+rule assign_sex_chrom_copy:
+    """Apply the revised sex-chromosome ploidy HMM to the pre-processed BAF data for this embryo and output a TSV."""
+    input:
+        baf_pkl="results/natera_inference/{mother_id}+{father_id}/{child_id}.sex_chroms.bafs.pkl.gz",
+    output:
+        karyo_tsv="results/natera_inference/{mother_id}+{father_id}/{child_id}.sex_chroms.total_ploidy.tsv",
+    resources:
+        time="0:30:00",
+        mem_mb="4G",
+    params:
+        mother_id=lambda wildcards: f"{wildcards.mother_id}",
+        father_id=lambda wildcards: f"{wildcards.father_id}",
+        child_id=lambda wildcards: f"{wildcards.child_id}",
+        chroms=chroms,
+    script:
+        "scripts/sex_chrom_copy.py"
