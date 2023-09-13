7efd1d06689d2b243bba307168e31d048b7da91a
scarios1 <scarios1@login03.cm.cluster>
2023-09-13 16:43:36 -0400

    count embryos per mother with weighted age

---
+++ analysis/phenotypes/scripts/embryo_count_by_mother.R
@@ -0,0 +1,47 @@
+## Make embryo count phenotype for use in GWAS 
+
+# load libraries
+library(data.table)
+library(dplyr)
+library(tidyr)
+
+# Usage: ./embryo_count_by_mother.R \ 
+# /data/rmccoy22/natera_spectrum/data/summary_metadata/spectrum_metadata_merged.csv \
+# /scratch16/rmccoy22/scarios1/natera_aneuploidy/analysis/phenotypes/embryo_count_mother.csv
+
+# accept args from snakemake
+args = commandArgs(trailingOnly = TRUE)
+metadata_fp <- args[1]
+out_fname <- args[2]
+
+# read in metadata 
+metadata <- fread(metadata_fp)
+
+# create new column that tags every individual affiliated with each mother even if in different caseIDs
+metadata_merged_array <- metadata %>%
+  mutate(array_id_merged = ifelse(family_position == "mother", paste0(array, "_merged"), NA_character_)) %>%
+  group_by(casefile_id) %>%
+  fill(array_id_merged) %>%
+  ungroup() 
+
+# create dataframe that calculates the weighted age (based on age at each embryo), number of embryos, and number of visits 
+weighted_ages <- metadata_merged_array %>%
+  filter(family_position == "child") %>%
+  group_by(array_id_merged) %>%
+  summarise(weighted_age = sum(patient_age) / n(),
+            child_count = n(), 
+            num_visits = length(unique(patient_age))) %>% 
+  as.data.frame()
+
+# remove "_merged" from array column to allow easier downstream intersection 
+weighted_ages$array <- gsub('_merged', '', weighted_ages$array_id_merged)
+  
+# write to file 
+fwrite(weighted_ages, out_fname)
+
+# check age range of multi-visit mothers 
+test_age_range <- metadata_merged_array %>%
+  filter(family_position == "child") %>%
+  group_by(array_id_merged) %>%
+  summarise(range = max(patient_age) - min(patient_age)) %>% 
+  as.data.frame()
