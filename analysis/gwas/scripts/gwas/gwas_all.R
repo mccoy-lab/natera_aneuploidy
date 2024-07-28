ecf05ce16e8cb8f133f8280347f71aeba269c402
scarios1 <scarios1@devrmccoy1.cm.cluster>
2024-07-28 11:49:34 -0400

    add donor ages and source for gwas

--- analysis/gwas/scripts/gwas/gwas_all.R
+++ analysis/gwas/scripts/gwas/gwas_all.R
@@ -104,6 +104,12 @@ get_gt <- function(bed, bed_dataset_indices, snp_index, metadata,
     merge(phenotype, by = "array") %>%
     merge(pcs, by = "array") 
   
+  # Assign egg and sperm donor ages 
+  # get average egg donor age from https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7530253/
+  # get average sperm donor age from https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9118971/#:~:text=Donors%20were%20aged%2027%20years,aged%2030%20years%20and%20younger.
+  gt[egg_donor == "yes", patient_age_cycle := 25]
+  gt[sperm_donor == "yes", partner_age_cycle := 27]
+  
   return(gt)
 }
 
